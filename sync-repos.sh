572472f9dc2ea9dc1b59f81021a649574d53818e
Luke Pafford <lukepafford@gmail.com>
2019-08-20 15:21:33 -0700

    Create sync-repos.sh

---
+++ sync-repos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [[ "$#" -ne 2 ]]; then
+        echo "$0 repoFile destination"
+        exit 1
+fi
+
+if ! rpm -q yum-utils > /dev/null 2>&1; then
+        yum -y install yum-utils
+fi
+
+repoFile="$1"
+dest="$2"
+
+for repoId in $(cat "$repoFile"); do
+        reposync $repoId --repoid=$repoId --downloadcomps --download-metadata --newest-only --download_path=$dest
+done
