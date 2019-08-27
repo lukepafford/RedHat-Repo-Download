9c7bc9a629f994437505a2e21c307966480624a2
Luke Pafford <lukepafford@gmail.com>
2019-08-27 16:57:35 -0700

    Update transformation to handle releasever better
    If I want to handle this perfectly, I need to generate metadata about the yum variables during the time i reposync, and then read those values later. This is a quick fix to an issue that I might just keep for my needs, but can potentially be buggy for other users.

--- sync_patches/transform-satellite-repo-mapping.py
+++ sync_patches/transform-satellite-repo-mapping.py
@@ -21,22 +21,31 @@ def create_repositories(csv_file):
   with open(csv_file) as f:
     r = reader(f)
     for line in r:
-      yield { 'repository_id': line[0], 'content_url': line[1] }
+      repository_id = line[0]
+      content_url = line[1]
+      yield (repository_id, content_url)
       
 def render_content_urls(repo, yum_vars, baseDest):
   """ Replace content_url variables with the expanded path """
   patterns = [
-    (re.compile('\$releasever'), 'releasever'),
     (re.compile('\$basearch'), 'basearch'),
     (re.compile('\$arch'), 'arch'),
     (re.compile('\$uuid'), 'uuid')
   ]
-  for r in repo:
-    content_url = r['content_url'] # create a new content_url that we will transform
+  for repository_id, content_url in repo:
+    new_content_url = content_url # create a new content_url that we will transform
     for pattern in patterns: # apply all patterns to our content_url
-      content_url = re.sub(pattern[0], yum_vars[pattern[1]], content_url) # Replace any instances of yum variables with the real value
-    content_url = os.path.join(baseDest, content_url[1:]) # Strip first '/' character so join works properly
-    yield (r['repository_id'], content_url) # Return tuple with valid dest
+      new_content_url = re.sub(pattern[0], yum_vars[pattern[1]], new_content_url) # Replace any instances of yum variables with the real value
+    
+    # Brue force set the releasever. Will likely work 99.9% of the time
+    # If a repo isn't for 7Workstation or 7Server, then this will fail
+    if 'workstation' in repository_id.lower():
+      new_content_url = re.sub('\$releasever', '7Workstation', new_content_url)
+    else:
+      new_content_url = re.sub('\$releasever', '7Server', new_content_url)
+    
+    new_content_url = os.path.join(baseDest, new_content_url[1:]) # Strip first '/' character so join works properly
+    yield (repository_id, new_content_url) # Return tuple with valid dest
     
 def write_json(repo_dict, dest):
   with open(dest, 'w') as f:
