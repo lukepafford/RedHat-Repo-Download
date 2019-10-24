9960e53cecc08467c603b8228d988e8ca575e19a
Luke Pafford <lukepafford@gmail.com>
2019-10-23 20:10:29 -0400

    Update openshift_images to contain component images and document the module

--- docker/openshift_images.py
+++ docker/openshift_images.py
@@ -1,13 +1,163 @@
 #!/usr/bin/python3
+""" 
+Purpose
+========
+
+This module does nothing other than generate lists which contain URLS
+of container images that can be downloaded from RedHats registry.
+
+This module was created so that it would be easy to modify and extend any images to these lists
+with minimal hassle.
+
+
+
+Variables
+==========
+User code only needs to concern itself with `component_images`, and `user_images`.
+
+
+* `component_images` and `component_images_no_changes` lists are created from
+  https://docs.openshift.com/container-platform/3.11/install/disconnected_install.html#disconnected-syncing-images
+
+* `system_tag_version` is the shared tag used by all required OpenShift component images
+
+* `registry` is the domain name of the image registry
+
+* `user_images` is a list of image URLs that OpenShift expects to have in its registry
+
+* `image_map` is the dictionary that is used to build the URLs in `user_images`
+
+"""
 from typing import List, Dict, NewType, Union
 
 system_tag_version: str = 'v3.11.135'
 registry = 'registry.redhat.io'
 
+
+component_images: List[str] = [
+	'registry.redhat.io/openshift3/apb-base',
+	'registry.redhat.io/openshift3/apb-tools',
+	'registry.redhat.io/openshift3/automation-broker-apb',
+	'registry.redhat.io/openshift3/csi-attacher',
+	'registry.redhat.io/openshift3/csi-driver-registrar',
+	'registry.redhat.io/openshift3/csi-livenessprobe',
+	'registry.redhat.io/openshift3/csi-provisioner',
+	'registry.redhat.io/openshift3/grafana',
+	'registry.redhat.io/openshift3/local-storage-provisioner',
+	'registry.redhat.io/openshift3/manila-provisioner',
+	'registry.redhat.io/openshift3/mariadb-apb',
+	'registry.redhat.io/openshift3/mediawiki',
+	'registry.redhat.io/openshift3/mediawiki-apb',
+	'registry.redhat.io/openshift3/mysql-apb',
+	'registry.redhat.io/openshift3/ose-ansible-service-broker',
+	'registry.redhat.io/openshift3/ose-cli',
+	'registry.redhat.io/openshift3/ose-cluster-autoscaler',
+	'registry.redhat.io/openshift3/ose-cluster-capacity',
+	'registry.redhat.io/openshift3/ose-cluster-monitoring-operator',
+	'registry.redhat.io/openshift3/ose-console',
+	'registry.redhat.io/openshift3/ose-configmap-reloader',
+	'registry.redhat.io/openshift3/ose-control-plane',
+	'registry.redhat.io/openshift3/ose-deployer',
+	'registry.redhat.io/openshift3/ose-descheduler',
+	'registry.redhat.io/openshift3/ose-docker-builder',
+	'registry.redhat.io/openshift3/ose-docker-registry',
+	'registry.redhat.io/openshift3/ose-efs-provisioner',
+	'registry.redhat.io/openshift3/ose-egress-dns-proxy',
+	'registry.redhat.io/openshift3/ose-egress-http-proxy',
+	'registry.redhat.io/openshift3/ose-egress-router',
+	'registry.redhat.io/openshift3/ose-haproxy-router',
+	'registry.redhat.io/openshift3/ose-hyperkube',
+	'registry.redhat.io/openshift3/ose-hypershift',
+	'registry.redhat.io/openshift3/ose-keepalived-ipfailover',
+	'registry.redhat.io/openshift3/ose-kube-rbac-proxy',
+	'registry.redhat.io/openshift3/ose-kube-state-metrics',
+	'registry.redhat.io/openshift3/ose-metrics-server',
+	'registry.redhat.io/openshift3/ose-node',
+	'registry.redhat.io/openshift3/ose-node-problem-detector',
+	'registry.redhat.io/openshift3/ose-operator-lifecycle-manager',
+	'registry.redhat.io/openshift3/ose-ovn-kubernetes',
+	'registry.redhat.io/openshift3/ose-pod',
+	'registry.redhat.io/openshift3/ose-prometheus-config-reloader',
+	'registry.redhat.io/openshift3/ose-prometheus-operator',
+	'registry.redhat.io/openshift3/ose-recycler',
+	'registry.redhat.io/openshift3/ose-service-catalog',
+	'registry.redhat.io/openshift3/ose-template-service-broker',
+	'registry.redhat.io/openshift3/ose-tests',
+	'registry.redhat.io/openshift3/ose-web-console',
+	'registry.redhat.io/openshift3/postgresql-apb',
+	'registry.redhat.io/openshift3/registry-console',
+	'registry.redhat.io/openshift3/snapshot-controller',
+	'registry.redhat.io/openshift3/snapshot-provisioner',
+	'registry.redhat.io/openshift3/ose-efs-provisioner',
+	'registry.redhat.io/openshift3/metrics-cassandra',
+	'registry.redhat.io/openshift3/metrics-hawkular-metrics',
+	'registry.redhat.io/openshift3/metrics-hawkular-openshift-agent',
+	'registry.redhat.io/openshift3/metrics-heapster',
+	'registry.redhat.io/openshift3/metrics-schema-installer',
+	'registry.redhat.io/openshift3/oauth-proxy',
+	'registry.redhat.io/openshift3/ose-logging-curator5',
+	'registry.redhat.io/openshift3/ose-logging-elasticsearch5',
+	'registry.redhat.io/openshift3/ose-logging-eventrouter',
+	'registry.redhat.io/openshift3/ose-logging-fluentd',
+	'registry.redhat.io/openshift3/ose-logging-kibana5',
+	'registry.redhat.io/openshift3/prometheus',
+	'registry.redhat.io/openshift3/prometheus-alertmanager',
+	'registry.redhat.io/openshift3/prometheus-node-exporter',
+	'registry.redhat.io/jboss-amq-6/amq63-openshift',
+	'registry.redhat.io/jboss-datagrid-7/datagrid71-openshift',
+	'registry.redhat.io/jboss-datagrid-7/datagrid71-client-openshift',
+	'registry.redhat.io/jboss-datavirt-6/datavirt63-openshift',
+	'registry.redhat.io/jboss-datavirt-6/datavirt63-driver-openshift',
+	'registry.redhat.io/jboss-decisionserver-6/decisionserver64-openshift',
+	'registry.redhat.io/jboss-processserver-6/processserver64-openshift',
+	'registry.redhat.io/jboss-eap-6/eap64-openshift',
+	'registry.redhat.io/jboss-eap-7/eap71-openshift',
+	'registry.redhat.io/jboss-webserver-3/webserver31-tomcat7-openshift',
+	'registry.redhat.io/jboss-webserver-3/webserver31-tomcat8-openshift',
+	'registry.redhat.io/openshift3/jenkins-2-rhel7',
+	'registry.redhat.io/openshift3/jenkins-agent-maven-35-rhel7',
+	'registry.redhat.io/openshift3/jenkins-agent-nodejs-8-rhel7',
+	'registry.redhat.io/openshift3/jenkins-slave-base-rhel7',
+	'registry.redhat.io/openshift3/jenkins-slave-maven-rhel7',
+	'registry.redhat.io/openshift3/jenkins-slave-nodejs-rhel7',
+	'registry.redhat.io/rhscl/mongodb-32-rhel7',
+	'registry.redhat.io/rhscl/mysql-57-rhel7',
+	'registry.redhat.io/rhscl/perl-524-rhel7',
+	'registry.redhat.io/rhscl/php-56-rhel7',
+	'registry.redhat.io/rhscl/postgresql-95-rhel7',
+	'registry.redhat.io/rhscl/python-35-rhel7',
+	'registry.redhat.io/redhat-sso-7/sso70-openshift',
+	'registry.redhat.io/rhscl/ruby-24-rhel7',
+	'registry.redhat.io/redhat-openjdk-18/openjdk18-openshift',
+	'registry.redhat.io/redhat-sso-7/sso71-openshift',
+	'registry.redhat.io/rhscl/nodejs-6-rhel7',
+	'registry.redhat.io/rhscl/mariadb-101-rhel7',
+]
+
+component_images_no_changes: List[str] = [
+	'registry.redhat.io/rhel7/etcd:3.2.22',
+	'registry.redhat.io/cloudforms46/cfme-openshift-postgresql',
+	'registry.redhat.io/cloudforms46/cfme-openshift-memcached',
+	'registry.redhat.io/cloudforms46/cfme-openshift-app-ui',
+	'registry.redhat.io/cloudforms46/cfme-openshift-app',
+	'registry.redhat.io/cloudforms46/cfme-openshift-embedded-ansible',
+	'registry.redhat.io/cloudforms46/cfme-openshift-httpd',
+	'registry.redhat.io/cloudforms46/cfme-httpd-configmap-generator',
+	'registry.redhat.io/rhgs3/rhgs-server-rhel7',
+	'registry.redhat.io/rhgs3/rhgs-volmanager-rhel7',
+	'registry.redhat.io/rhgs3/rhgs-gluster-block-prov-rhel7',
+	'registry.redhat.io/rhgs3/rhgs-s3-server-rhel7',
+]
+
+
+
+
 Namespace = str
 Repository = str
 Tags = List[str]
 
+# This mapping is built from the openshift cockpit console "Cluster > Images" section
+# Any image it expects to see is included in this dictionary
 image_map: Dict[Namespace, Dict[Repository, Tags]] = {
 	'fuse7': {
 		'fuse-apicurito': ['1.2','1.3'],
@@ -227,6 +377,7 @@ image_map: Dict[Namespace, Dict[Repository, Tags]] = {
 }
 
 
+# Build the fqdn for the user images
 user_images: List[str] = []
 for namespace, repositories in image_map.items():
 	for repository, tags in repositories.items():
