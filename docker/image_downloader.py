393fb260c0e94527f9ae402d44a6cc00ccd52eca
Luke Pafford <lukepafford@gmail.com>
2019-10-24 19:58:53 -0400

    Changed cache implementation to remove bugs

--- docker/image_downloader.py
+++ docker/image_downloader.py
@@ -4,29 +4,22 @@ from typing import List
 from pathlib import Path
 from subprocess import run
 import sys
-import json
-from json.decoder import JSONDecodeError
 
 all_images: List[str] = user_images + component_images
-cache_name: Path = Path(sys.argv[0]).resolve().parent / '.images.json'
+cache_name: Path = Path(sys.argv[0]).resolve().parent / '.images.txt'
 
 with open(cache_name, 'a+') as cache_handle:
 	cache_handle.seek(0)
-	try:
-		cache: List[str] = json.load(cache_handle)
-	except JSONDecodeError:
-		cache = []
+	cache: List[str] = [ line.rstrip() for line in cache_handle.readlines() ]
 
 	for image in all_images:
 		if image not in cache:
 			res = run(['/bin/docker', 'pull', image])
 			if res.returncode != 0:
-				# We had an error, Save results so we don't repeat on next run
-				# and start back up on the image that caused the error
-				json.dump(cache, cache_handle)	
 				raise Exception(f'Failed to pull image {image}')
 			else:
-				cache.append(image)	
+				# Save successful results to the cache
+				cache_handle.write(f'{image}\n')
 
 	# Wipe the cache if all processes succeeded
-	json.dump([], cache_handle)
+	cache_handle.truncate()
