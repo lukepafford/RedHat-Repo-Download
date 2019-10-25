e68dd4eb13e5e729fe74b2b8731e374358531d71
Luke Pafford <lukepafford@gmail.com>
2019-10-25 17:46:26 -0400

    Split up functionality of image downloader

--- docker/image_downloader.py
+++ docker/image_downloader.py
@@ -2,24 +2,66 @@
 from openshift_images import user_images, component_images
 from typing import List
 from pathlib import Path
-from subprocess import run
+from subprocess import run, PIPE
+from functools import wraps
 import sys
 
-all_images: List[str] = user_images + component_images
-cache_name: Path = Path(sys.argv[0]).resolve().parent / '.images.txt'
+def main():
+	all_images: List[str] = user_images + component_images
+	cache_path: Path = Path(sys.argv[0]).resolve().parent / '.images.txt'
 
-with open(cache_name, 'a+') as cache_handle:
-	cache_handle.seek(0)
-	cache: List[str] = [ line.rstrip() for line in cache_handle.readlines() ]
 
+	@cache_line_entries(cache_path)
+	def process_image(image):
+		pull_image(image)
+
+
+	# Process all images
 	for image in all_images:
-		if image not in cache:
-			res = run(['/bin/docker', 'pull', image])
-			if res.returncode != 0:
-				raise Exception(f'Failed to pull image {image}')
-			else:
-				# Save successful results to the cache
-				cache_handle.write(f'{image}\n')
+		process_image(image)
 
-	# Wipe the cache if all processes succeeded
-	cache_handle.truncate()
+
+def cache_line_entries(cache_path):
+	"""
+	Writes each item to a single line in a file. If the item
+	already exists in the file then skip it. If the item doesn't exist,
+	then process the item, and add the entry to the cache. If the program
+	ends before processing all the items in the list, then it will resume
+	where it left off.
+	"""
+	def decorator(process):
+		@wraps(process)
+		def inner(item):
+			with open(cache_path, 'a+') as cache_handle:
+				cache_handle.seek(0)
+				cache: List[str] = [ line.rstrip() for line in cache_handle.readlines() ]
+				if item not in cache:
+					process(item)
+					cache_handle.write(f'{item}\n')
+		return inner
+	return decorator
+
+
+def pull_image(url: str, retries: int = 3):
+	res = run(['/bin/docker', 'pull', url], stderr=PIPE, universal_newlines=True)
+
+	# Exit on successful pull
+	if res.returncode == 0:
+		print(res.stdout)
+		return
+
+
+	# This is the main error we want to fix, we likely have a typo in one of our url urls
+	elif res.stderr.lower().strip().endswith('not found'):
+		raise Exception(f'Failed to pull url {url}. Check the spelling of the url')
+
+	# Retry the download if it's any other error, the error is likely on the server end
+	elif retries > 0:
+		pull_image(url, retries - 1)
+
+	else:
+		raise Exception(f'Failed to pull url {url}. Error: {res.stderr}')
+
+
+if __name__ == '__main__':
+	main()
